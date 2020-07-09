59b6b560423cff3af46250eef9b1353be882f4c7
Luca Stasio <joshuagame@localhost.localdomain>
2020-07-09 08:34:38 +0200

    final config package development

--- controller.go
+++ controller.go
@@ -1,18 +1,6 @@
 package core
 
-/*
-// register the handlers or handler_funcs by name in a map:
-handlerMap := make(map[string]*http.Handler)
-// OR
-handlerFuncMap := make(map[string]func(http.ResponseWriter, *http.Request))
-
-handlerMap["myHandler"] = myHandler
-
-// now you can iterate over you config values and assign them to a router
-for path, handler := range routes {
-    myRouter.Handler(path, handlerMap[handler])
-}
-*/
+// Controller .
 type Controller interface {
 	BasePath() string
 	Route(r *Router)
@@ -30,5 +18,4 @@ func (bc BaseController) BasePath() string {
 
 // Route .
 func (bc BaseController) Route(r *Router) {
-
 }
